71218c3abf883650fd2ab8b439dfe6051ae0e078
japiirainen <joona.piirainen@gmail.com>
2020-12-30 19:54:13 +0200

    db connection

---
+++ db/db.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/japiirainen/go-oluet-api/helpers"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func Connect() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("%s", err)
+		panic("env not found")
+	}
+
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("PG_HOST"),
+		helpers.ToInt(os.Getenv("PG_PORT")),
+		os.Getenv("PG_USER"),
+		os.Getenv("PG_PASSWORD"),
+		os.Getenv("PG_DBNAME"))
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	defer db.Close()
+}
+
+func main() {
+	Connect()
+}
