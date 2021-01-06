2131365473990c24eaf18942fe16c197e1ebe0d2
japiirainen <joona.piirainen@gmail.com>
2021-01-06 15:39:53 +0200

    add gorilla mux

--- go.mod
+++ go.mod
@@ -5,12 +5,24 @@ go 1.15
 require (
 	github.com/360EntSecGroup-Skylar/excelize/v2 v2.3.1
 	github.com/99designs/gqlgen v0.13.0
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.1 // indirect
 	github.com/joho/godotenv v1.3.0
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.9.0
 	github.com/mitchellh/mapstructure v0.0.0-20180220230111-00c29f56e238 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/sirupsen/logrus v1.7.0
+	github.com/ugorji/go v1.2.2 // indirect
 	github.com/vektah/gqlparser/v2 v2.1.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/net v0.0.0-20201029221708-28c70e62bb1d // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	golang.org/x/sys v0.0.0-20210105210732-16f7687f5001 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- server.go
+++ server.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/mux"
 	"github.com/japiirainen/go-oluet-api/db"
 	"github.com/japiirainen/go-oluet-api/gql/generated"
 	"github.com/japiirainen/go-oluet-api/gql/resolvers"
@@ -15,6 +22,10 @@ import (
 const defaultPort = "5000"
 
 func main() {
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
 	log.SetFormatter(&log.TextFormatter{})
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -22,13 +33,41 @@ func main() {
 	}
 	c := db.Connect()
 	defer c.CloseConnection()
+	r := mux.NewRouter()
+
+	//internal routes
+	r.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	})
+
+	//graphql routes
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolvers.Resolver{
 		DB: c,
 	}}))
+	r.Handle("/graphql", playground.Handler("GraphQL playground", "/query"))
+	r.Handle("/graphql/query", srv)
+	log.Infof("playground @ -> http://localhost:%s/graphql", port)
+	log.Infof("query graphql @ -> http://localhost:%s/graphql/query", port)
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	s := &http.Server{
+		Handler: r,
+		Addr:    fmt.Sprintf("127.0.0.1:%s", port),
 
-	log.Infof("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	s.Shutdown(ctx)
+	log.Println("shutting down")
+	os.Exit(0)
 }
